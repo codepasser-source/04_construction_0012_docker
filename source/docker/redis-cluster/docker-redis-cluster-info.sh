b3fccf60eb18ce8440c75944ab8863cca0a7cecc
Joker.cheng <codepasser@outlook.com>
2020-04-17 17:34:01 +0800

    docker build shell: redis cluster

---
+++ source/docker/redis-cluster/docker-redis-cluster-info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
+# docker exec -it redis-node-01 redis-cli -c -p 6379 -a cde3VFR$
+docker exec -it redis-node-01 redis-cli -p 6379 cluster info
+docker exec -it redis-node-01 redis-cli -p 6379 cluster nodes
