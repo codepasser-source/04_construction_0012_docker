f1b7abb7203a591d7abcf109e27b8fb8b3e524cf
Joker.cheng <codepasser@outlook.com>
2020-04-17 18:05:35 +0800

    docker build shell: redis cluster

--- source/docker/redis-cluster/docker-redis-cli-node-01.sh
+++ source/docker/redis-cluster/docker-redis-cli-node-01.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
+# requirepass when mast to be setting password excute
 # docker exec -it redis-node-01 redis-cli -c -p 6379 -a cde3VFR$
 docker exec -it redis-node-01 redis-cli -c -p 6379
