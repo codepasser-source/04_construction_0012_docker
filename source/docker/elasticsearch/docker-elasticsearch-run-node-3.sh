83bdb076d677c8aa9207c1f93a5769962b0f0a0d
Joker.cheng <codepasser@outlook.com>
2020-04-17 15:52:24 +0800

    docker build shell: es upgrade plugins

--- source/docker/elasticsearch/docker-elasticsearch-run-node-3.sh
+++ source/docker/elasticsearch/docker-elasticsearch-run-node-3.sh
@@ -6,7 +6,7 @@ docker run \
   -p 9303:9303 \
   -e "ES_JAVA_OPTS=-Xms1024m -Xmx1024m" \
   -v /data/docker/elasticsearch/volume/conf/elasticsearch-node-3.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
-  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-ik-6.4.3:/usr/share/elasticsearch/plugins/elasticsearch-analysis-ik-6.4.3 \
-  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-pinyin-6.4.3:/usr/share/elasticsearch/plugins/elasticsearch-analysis-pinyin-6.4.3 \
+  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-ik-7.6.2:/usr/share/elasticsearch/plugins/elasticsearch-analysis-ik-7.6.2 \
+  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-pinyin-7.6.2:/usr/share/elasticsearch/plugins/elasticsearch-analysis-pinyin-7.6.2 \
   -v /data/docker/elasticsearch/volume/data/node-3:/usr/share/elasticsearch/data \
   -d codepasser/elasticsearch
