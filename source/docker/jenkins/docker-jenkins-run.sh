0bdace6b9f1ddaf8a9f19714c33e544714bfa257
Joker.cheng <codepasser@outlook.com>
2020-05-07 10:53:31 +0800

    docker build shell: jenkins volumes

--- source/docker/jenkins/docker-jenkins-run.sh
+++ source/docker/jenkins/docker-jenkins-run.sh
@@ -1,7 +1,7 @@
 # docker run [OPTIONS] IMAGE [COMMAND] [ARG...]
 docker run \
-	--name jenkins-master \
-	-p 8081:8080 \
-	-p 50000:50000 \
-	-v $HOME/jenkins/volume/data/:/var/jenkins_home \
-	-d codepasser/jenkins
+  --name jenkins-master \
+  -p 8081:8080 \
+  -p 50000:50000 \
+  -v /data/docker/jenkins/volume/data/:/var/jenkins_home \
+  -d codepasser/jenkins
