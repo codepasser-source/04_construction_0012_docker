7518c05d5b948c03336247e579df02aabef90a83
Joker.cheng <codepasser@outlook.com>
2020-05-15 14:11:05 +0800

    docker build shell: elastic docker version upgrade 7.7.0

--- source/docker/elasticsearch/docker-elasticsearch-run-node-1.sh
+++ source/docker/elasticsearch/docker-elasticsearch-run-node-1.sh
@@ -6,7 +6,7 @@ docker run \
   -p 9301:9301 \
   -e "ES_JAVA_OPTS=-Xms1024m -Xmx1024m" \
   -v /data/docker/elasticsearch/volume/conf/elasticsearch-node-1.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
-  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-ik-7.6.2:/usr/share/elasticsearch/plugins/elasticsearch-analysis-ik-7.6.2 \
-  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-pinyin-7.6.2:/usr/share/elasticsearch/plugins/elasticsearch-analysis-pinyin-7.6.2 \
+  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-ik-7.7.0:/usr/share/elasticsearch/plugins/elasticsearch-analysis-ik-7.7.0 \
+  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-pinyin-7.7.0:/usr/share/elasticsearch/plugins/elasticsearch-analysis-pinyin-7.7.0 \
   -v /data/docker/elasticsearch/volume/data/node-1:/usr/share/elasticsearch/data \
   -d codepasser/elasticsearch
--- source/docker/elasticsearch/docker-elasticsearch-run-node-3.sh
+++ source/docker/elasticsearch/docker-elasticsearch-run-node-3.sh
@@ -6,7 +6,7 @@ docker run \
   -p 9303:9303 \
   -e "ES_JAVA_OPTS=-Xms1024m -Xmx1024m" \
   -v /data/docker/elasticsearch/volume/conf/elasticsearch-node-3.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
-  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-ik-7.6.2:/usr/share/elasticsearch/plugins/elasticsearch-analysis-ik-7.6.2 \
-  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-pinyin-7.6.2:/usr/share/elasticsearch/plugins/elasticsearch-analysis-pinyin-7.6.2 \
+  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-ik-7.7.0:/usr/share/elasticsearch/plugins/elasticsearch-analysis-ik-7.7.0 \
+  -v /data/docker/elasticsearch/volume/plugins/elasticsearch-analysis-pinyin-7.7.0:/usr/share/elasticsearch/plugins/elasticsearch-analysis-pinyin-7.7.0 \
   -v /data/docker/elasticsearch/volume/data/node-3:/usr/share/elasticsearch/data \
   -d codepasser/elasticsearch
