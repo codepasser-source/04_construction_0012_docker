9034c94b02ae293a07232814ea317fda07556e4d
Joker.cheng <codepasser@outlook.com>
2020-07-30 15:11:28 +0800

    docker build shell: fixed redis cli

--- source/docker/redis-cluster/docker-redis-cli-node-01.sh
+++ source/docker/redis-cluster/docker-redis-cli-node-01.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-# requirepass when mast to be setting password excute
-# docker exec -it redis-node-01 redis-cli -c -p 6379 -a cde3VFR$
-docker exec -it redis-node-01 redis-cli -c -p 6379
+docker exec -it redis-node-01 redis-cli -c -p 6379 -a cde3VFR$
+# docker exec -it redis-node-01 redis-cli -c -p 6379
--- source/docker/redis-cluster/docker-redis-cli-node-13.sh
+++ source/docker/redis-cluster/docker-redis-cli-node-13.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-# docker exec -it redis-node-11 redis-cli -c -p 6391 -a cde3VFR$
-docker exec -it redis-node-13 redis-cli -c -p 6391
+docker exec -it redis-node-13 redis-cli -c -p 6391 -a cde3VFR$
+# docker exec -it redis-node-13 redis-cli -c -p 6391
