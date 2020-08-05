410b6cf098dc72f0486cfd92c15dcdf33bcff58b
Joker.cheng <codepasser@outlook.com>
2020-08-05 15:39:15 +0800

    docker build shell: upgrade mongodb node single

--- source/docker/mongo-cluster/docker-mongodb-run-node-single.sh
+++ source/docker/mongo-cluster/docker-mongodb-run-node-single.sh
@@ -6,6 +6,7 @@ docker run \
   -e MONGO_INITDB_ROOT_USERNAME=admin \
   -e MONGO_INITDB_ROOT_PASSWORD=Sa*963.-+ \
   -v /data/docker/mongo/volume/config/mongod-single.conf:/etc/mongo/mongod.conf \
+  -v /data/docker/mongo/volume/configdb/node-single/:/data/configdb \
   -v /data/docker/mongo/volume/data/node-single:/data/db \
   -d codepasser/mongo \
   --config /etc/mongo/mongod.conf
