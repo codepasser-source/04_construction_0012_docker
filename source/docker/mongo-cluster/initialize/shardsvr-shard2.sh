e69c82fd2d1b14646d71ce11c7fd5bd03ba46b0c
Joker.cheng <codepasser@outlook.com>
2020-08-06 10:32:42 +0800

    docker build shell: upgrade mongodb node shardsvr

--- source/docker/mongo-cluster/initialize/shardsvr-shard2.sh
+++ source/docker/mongo-cluster/initialize/shardsvr-shard2.sh
@@ -1,6 +1,6 @@
 docker exec -it mongo-shardsvr-21 mongo
 config = {
-    "_id":"shard1",
+    "_id":"shard2",
     "members":[
       {"_id":0,host:"172.16.20.121:27217","arbiterOnly":true},
       {"_id":1,host:"172.16.20.121:27227"},
--- source/docker/mongo-cluster/initialize/shardsvr-shard3.sh
+++ source/docker/mongo-cluster/initialize/shardsvr-shard3.sh
@@ -1,6 +1,6 @@
 docker exec -it mongo-shardsvr-31 mongo
 config = {
-    "_id":"shard1",
+    "_id":"shard3",
     "members":[
       {"_id":0,host:"172.16.20.121:27317"},
       {"_id":1,host:"172.16.20.121:27327","arbiterOnly":true},
