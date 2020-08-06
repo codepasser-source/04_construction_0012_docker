292c052c94821fecffd802704d06342b91fbefc0
Joker.cheng <codepasser@outlook.com>
2020-08-06 14:55:17 +0800

    docker build shell: upgrade mongodb node mongos

--- source/docker/mongo-cluster/docker-mongodb-cli.sh
+++ source/docker/mongo-cluster/docker-mongodb-cli.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-docker exec -it [CONTAINER] mongo
-#docker exec -it mongo-single mongo daemon -u daemon -p cde3VFR$
-#docker exec -it mongo-single mongo admin -u root -p Sa*963.-+
+#docker exec -it [CONTAINER] mongo
+# single auth
+docker exec -it mongo-single mongo admin -u root -p Sa*963.-+
+docker exec -it mongo-single mongo daemon -u daemon -p cde3VFR$
+# mongos-01 auth
+docker exec -it mongo-mongos-01 mongo admin -u root -p Sa*963.-+
+docker exec -it mongo-mongos-01 mongo daemon -u daemon -p cde3VFR$
+# mongos-02 auth
+docker exec -it mongo-mongos-02 mongo admin -u root -p Sa*963.-+
+docker exec -it mongo-mongos-02 mongo daemon -u daemon -p cde3VFR$
+# mongos-03 auth
+docker exec -it mongo-mongos-03 mongo admin -u root -p Sa*963.-+
+docker exec -it mongo-mongos-03 mongo daemon -u daemon -p cde3VFR$
