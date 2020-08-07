6fe6628b908a2bf17d01090c7b4858b9e3d5429b
Joker.cheng <codepasser@outlook.com>
2020-08-07 10:50:02 +0800

    docker build shell: upgrade mongodb docker configuration

--- source/docker/mongo/docker-mongodb-auth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-docker exec -it mongo-master mongo admin
-## execute create user
-##db.createUser({ user: 'root', pwd: 'Sa*963.-+', roles: [ { role: "root", db: "admin" } ] });
-## execute create daemon
-##use daemon
-##db.createUser({ user: 'daemon', pwd: 'cde3VFR$', roles: [ { role: "readWrite", db: "daemon" } ] });
--- source/docker/mongo/docker-mongodb-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#  docker stop [OPTIONS] CONTAINER [CONTAINER...]
-docker stop mongo-master
-# docker rm [OPTIONS] CONTAINER [CONTAINER...]
-docker rm mongo-master
