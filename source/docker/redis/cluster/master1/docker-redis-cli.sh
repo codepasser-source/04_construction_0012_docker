fae9f5389c9cced4128984a1c7b3bbe4eb01133b
codepasser <yangyang.cheng@valueonline.cn>
2021-09-09 16:09:17 +0800

    docker: redis upgrade

--- source/docker/redis/cluster/master1/docker-redis-cli.sh
+++ source/docker/redis/cluster/master1/docker-redis-cli.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-docker exec -it master1.redis.codepasser.io redis-cli -p 6379
+#docker exec -it master1.redis.codepasser.io redis-cli -p 6379
+docker exec -it master1.redis.codepasser.io redis-cli -p 6379 -c -a cde3VFR$
--- source/docker/redis/cluster/slave1/docker-redis-cli.sh
+++ source/docker/redis/cluster/slave1/docker-redis-cli.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-docker exec -it slave1.redis.codepasser.io redis-cli -p 6380
+#docker exec -it slave1.redis.codepasser.io redis-cli -p 6380
+docker exec -it slave1.redis.codepasser.io redis-cli -p 6380 -c -a cde3VFR$
