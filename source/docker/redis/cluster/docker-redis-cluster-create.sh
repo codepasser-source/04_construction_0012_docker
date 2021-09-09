446e7cb5c3665201a6fcf549536250910ef60646
codepasser <yangyang.cheng@valueonline.cn>
2021-09-09 12:07:41 +0800

    docker: redis upgrade

--- source/docker/redis/cluster/docker-redis-cluster-create.sh
+++ source/docker/redis/cluster/docker-redis-cluster-create.sh
@@ -3,7 +3,7 @@ docker exec -it master1.redis.codepasser.io redis-cli --cluster create \
   master1.redis.codepasser.io:6379 \
   master2.redis.codepasser.io:6379 \
   master3.redis.codepasser.io:6379 \
-  slave1.redis.codepasser.io:6380 \
-  slave2.redis.codepasser.io:6380 \
-  slave3.redis.codepasser.io:6380 \
+  slave1.redis.codepasser.io:6379 \
+  slave2.redis.codepasser.io:6379 \
+  slave3.redis.codepasser.io:6379 \
   --cluster-replicas 1
