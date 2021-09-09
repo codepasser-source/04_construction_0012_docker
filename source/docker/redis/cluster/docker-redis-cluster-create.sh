ee0d97dd1d231925aadf8054479d21c56314cd15
codepasser <yangyang.cheng@valueonline.cn>
2021-09-09 15:16:42 +0800

    docker: redis upgrade

--- source/docker/redis/cluster/docker-redis-cluster-create.sh
+++ source/docker/redis/cluster/docker-redis-cluster-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker exec -it master1.redis.codepasser.io redis-cli -h 8.8.8.100 -p 6379 --cluster create \
+docker exec -it master1.redis.codepasser.io redis-cli -p 6379 --cluster create \
   8.8.8.100:6379 \
   8.8.8.101:6379 \
   8.8.8.102:6379 \
