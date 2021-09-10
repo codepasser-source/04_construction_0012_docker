f88a65272e5eef60197057a2ac8e9122bc75e323
codepasser <yangyang.cheng@valueonline.cn>
2021-09-10 10:17:23 +0800

    docker: es upgrade

---
+++ source/docker/elasticsearch/single/docker-es-bash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
+docker exec -it single.es.codepasser.io bash
---
+++ source/docker/elasticsearch/single/docker-es-stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#  docker stop [OPTIONS] CONTAINER [CONTAINER...]
+docker stop single.es.codepasser.io
+# docker rm [OPTIONS] CONTAINER [CONTAINER...]
+docker rm single.es.codepasser.io
