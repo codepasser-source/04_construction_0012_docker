52b9ab068d68f2b4848b96c5d7decfaba6124f39
codepasser <yangyang.cheng@valueonline.cn>
2021-09-10 10:55:31 +0800

    docker: es upgrade

--- source/docker/elasticsearch/single/docker-es-run.sh
+++ source/docker/elasticsearch/single/docker-es-run.sh
@@ -6,7 +6,7 @@ docker run \
   -p 9200:9200 \
   -p 9300:9300 \
   -e "ES_JAVA_OPTS=-Xms1024m -Xmx1024m" \
-  -v $(pwd)/volume/conf/single.es.codepasser.io.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
+  -v $(pwd)/volume/conf/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
   -v $(pwd)/../plugins/elasticsearch-analysis-ik-7.14.0:/usr/share/elasticsearch/plugins/elasticsearch-analysis-ik-7.14.0 \
   -v $(pwd)/../plugins/elasticsearch-analysis-pinyin-7.14.0:/usr/share/elasticsearch/plugins/elasticsearch-analysis-pinyin-7.14.0 \
   -v single.es.codepasser.io:/usr/share/elasticsearch/data \
