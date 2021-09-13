c2c1db0953bbbfc0bd8f5726e7b8a553ae1d7ed0
codepasser <yangyang.cheng@valueonline.cn>
2021-09-13 17:01:09 +0800

    docker: mongo initialize shell

--- source/docker/mongo/cluster/mongos/node3/docker-mongodb-cli.sh
+++ source/docker/mongo/cluster/mongos/node3/docker-mongodb-cli.sh
@@ -2,4 +2,5 @@
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
 #docker exec -it [CONTAINER] mongo
 # single auth
-docker exec -it mongos3.mongo.codepasser.io mongo
+docker exec -it mongos3.mongo.codepasser.io mongo admin -u root -p Sa*963.-+
+#docker exec -it mongos3.mongo.codepasser.io mongo
