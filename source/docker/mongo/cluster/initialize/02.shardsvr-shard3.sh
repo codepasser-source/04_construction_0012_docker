573613fb902af8bca72ce341adc7d817847d80fe
codepasser <yangyang.cheng@valueonline.cn>
2021-09-13 16:55:00 +0800

    docker: mongo initialize shell

--- source/docker/mongo/cluster/initialize/02.shardsvr-shard3.sh
+++ source/docker/mongo/cluster/initialize/02.shardsvr-shard3.sh
@@ -1,10 +1,10 @@
-docker exec -it shard31.mongo.codepasser.io mongo
+docker exec -it shard33.mongo.codepasser.io mongo
 config = {
     "_id":"shard3",
     "members":[
       {"_id":0,host:"shard31.mongo.codepasser.io:27017"},
-      {"_id":1,host:"shard32.mongo.codepasser.io:27017"},
-      {"_id":2,host:"shard33.mongo.codepasser.io:27017","arbiterOnly":true}
+      {"_id":1,host:"shard32.mongo.codepasser.io:27017","arbiterOnly":true},
+      {"_id":2,host:"shard33.mongo.codepasser.io:27017"}
     ]
 }
 rs.initiate(config)
