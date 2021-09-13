d6fa541a6feede9d3429208c9e623fb32f9080f2
codepasser <yangyang.cheng@valueonline.cn>
2021-09-13 14:44:03 +0800

    docker: mongo upgrade

--- source/docker/mongo/single/docker-mongodb-cli.sh
+++ source/docker/mongo/single/docker-mongodb-cli.sh
@@ -3,5 +3,5 @@
 #docker exec -it [CONTAINER] mongo
 # single auth
 docker exec -it single.mongo.codepasser.io mongo admin -u root -p Sa*963.-+
-docker exec -it single.mongo.codepasser.io mongo admin -u admin -p Sa*963.-+
-docker exec -it single.mongo.codepasser.io mongo daemon -u daemon -p cde3VFR$
+#docker exec -it single.mongo.codepasser.io mongo admin -u admin -p Sa*963.-+
+#docker exec -it single.mongo.codepasser.io mongo daemon -u daemon -p cde3VFR$
