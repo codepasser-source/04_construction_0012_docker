6feecc910e6d7860c1f084bec13d8380c364bfde
codepasser <yangyang.cheng@valueonline.cn>
2022-03-11 13:40:01 +0800

    docker: zookeeper initialize shell

--- source/docker/zookeeper/single/docker-zookeeper-run.sh
+++ source/docker/zookeeper/single/docker-zookeeper-run.sh
@@ -4,4 +4,6 @@ docker run \
 	-p 2181:2181 \
 	-v $(pwd)/volume/conf/zoo.cfg:/conf/zoo.cfg \
 	-v single.zookeeper.codepasser.io:/data \
+	-v single.zookeeper.codepasser.io:/datalog \
+	-v single.zookeeper.codepasser.io:/logs \
 	-d codepasser/zookeeper
