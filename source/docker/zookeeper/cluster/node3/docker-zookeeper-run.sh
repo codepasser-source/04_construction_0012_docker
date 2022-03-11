2e8988e4b496362f4518c5f0b61427284173ee2c
codepasser <yangyang.cheng@valueonline.cn>
2022-03-11 13:51:49 +0800

    docker: zookeeper cluster shell

---
+++ source/docker/zookeeper/cluster/node3/docker-zookeeper-run.sh
@@ -0,0 +1,9 @@
+# docker run [OPTIONS] IMAGE [COMMAND] [ARG...]
+docker run \
+	--name node3.zookeeper.codepasser.io \
+	-p 2181:2181 \
+	-v $(pwd)/volume/conf/zoo.cfg:/conf/zoo.cfg \
+	-v $(pwd)/volume/data:/data \
+	-v node3.zookeeper.codepasser.io:/datalog \
+	-v node3.zookeeper.codepasser.io:/logs \
+	-d codepasser/zookeeper
