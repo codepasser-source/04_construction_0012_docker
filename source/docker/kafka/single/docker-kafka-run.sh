3011f1c620fa80871ac13fc17659b9487d37a615
codepasser <yangyang.cheng@valueonline.cn>
2022-03-12 14:34:18 +0800

    docker: kafka docker shell

--- source/docker/kafka/single/docker-kafka-run.sh
+++ source/docker/kafka/single/docker-kafka-run.sh
@@ -3,7 +3,11 @@ docker run \
 	--name single.kafka.codepasser.io \
 	--network codepasser_overlay \
 	-p 9092:9092 \
-	-e ALLOW_PLAINTEXT_LISTENER=yes \
+	-e KAFKA_BROKER_ID=1 \
+	-e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092 \
+	-e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://single.kafka.codepasser.io:9092 \
 	-e KAFKA_CFG_ZOOKEEPER_CONNECT=node1.zookeeper.codepasser.io:2181,node2.zookeeper.codepasser.io:2181,node3.zookeeper.codepasser.io:2181 \
-	-v single.kafka.codepasser.io:/bitnami/kafka \
+	-e ALLOW_PLAINTEXT_LISTENER=yes \
+	-v $(pwd)/volume/conf:/bitnami/kafka/config \
+	-v single.kafka.codepasser.io:/bitnami/kafka/data \
 	-d codepasser/kafka
