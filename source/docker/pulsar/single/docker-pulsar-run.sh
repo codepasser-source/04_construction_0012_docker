dbd633530109ce36b8dc08931d27b99929c5abbc
codepasser <yangyang.cheng@valueonline.cn>
2022-08-19 11:46:03 +0800

    docker: pulsar shell script

--- source/docker/pulsar/single/docker-pulsar-run.sh
+++ source/docker/pulsar/single/docker-pulsar-run.sh
@@ -1,9 +1,11 @@
 # docker run [OPTIONS] IMAGE [COMMAND] [ARG...]
 docker run \
-	--name single.pulsar.codepasser.io \
-	--network codepasser_overlay \
-	-p 9092:9092 \
-	-e ALLOW_PLAINTEXT_LISTENER=yes \
-	-v $(pwd)/volume/conf/server.properties:/bitnami/pulsar/config/server.properties \
-	-v single.pulsar.codepasser.io:/bitnami/pulsar/data \
-	-d codepasser/pulsar
+  --name single.pulsar.codepasser.io \
+  --network codepasser_overlay \
+  -p 6650:6650 \
+  -p 8080:8080 \
+  -v single.pulsar.codepasser.io:/pulsar/data \
+  -d codepasser/pulsar bin/pulsar standalone
+# -e PULSAR_PREFIX_xxx=yyy \
+# --mount source=pulsarconf,target=/pulsar/conf \
+# sh -c "bin/apply-config-from-env.py conf/standalone.conf && bin/pulsar standalone"
