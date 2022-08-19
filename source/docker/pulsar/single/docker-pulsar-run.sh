cc0d7a71569906895df67aa954279f32188b2c6d
codepasser <yangyang.cheng@valueonline.cn>
2022-08-19 14:00:15 +0800

    docker: pulsar shell script

--- source/docker/pulsar/single/docker-pulsar-run.sh
+++ source/docker/pulsar/single/docker-pulsar-run.sh
@@ -4,5 +4,6 @@ docker run \
   --network codepasser_overlay \
   -p 6650:6650 \
   -p 8080:8080 \
+  -v $(pwd)/volume/conf:/pulsar/conf \
   -v single.pulsar.codepasser.io:/pulsar/data \
   -d codepasser/pulsar bin/pulsar standalone
