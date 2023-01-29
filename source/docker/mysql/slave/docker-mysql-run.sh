c3dc9c25a54fb0bc912e08fa128f733428e7fd1e
yangyang.cheng <yangyang.cheng@valueonline.cn>
2023-01-29 16:31:03 +0800

    docker: mysql optimization conf

--- source/docker/mysql/slave/docker-mysql-run.sh
+++ source/docker/mysql/slave/docker-mysql-run.sh
@@ -3,6 +3,7 @@ docker run \
   --name slave.mysql.codepasser.io \
   --network codepasser_overlay \
   --security-opt seccomp:unconfined \
+  --restart=always \
   -p 3306:3306 \
   -e MYSQL_ROOT_PASSWORD=Sa*963.-+ \
   -v $(pwd)/volume/conf:/etc/mysql/conf.d \
