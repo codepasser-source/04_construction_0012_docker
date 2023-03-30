5ac5ae1bd9829d75e24ef0d0c999424109369eee
yangyang.cheng <yangyang.cheng@valueonline.cn>
2023-03-30 14:06:22 +0800

    docker: kafka cluster

--- source/docker/kafka/single/docker-kafka-run.sh
+++ source/docker/kafka/single/docker-kafka-run.sh
@@ -1,9 +1,9 @@
 # docker run [OPTIONS] IMAGE [COMMAND] [ARG...]
 docker run \
-	--name single.kafka.codepasser.io \
-	--network codepasser_overlay \
-	-p 9092:9092 \
-	-e ALLOW_PLAINTEXT_LISTENER=yes \
-	-v $(pwd)/volume/conf/server.properties:/bitnami/kafka/config/server.properties \
-	-v single.kafka.codepasser.io:/bitnami/kafka/data \
-	-d codepasser/kafka
+  --name single.kafka.codepasser.io \
+  --network codepasser_overlay \
+  -p 9092:9092 \
+  -e ALLOW_PLAINTEXT_LISTENER=yes \
+  -v $(pwd)/volume/conf/server.properties:/bitnami/kafka/config/server.properties \
+  -v single.kafka.codepasser.io:/bitnami/kafka/data \
+  -d codepasser/kafka
