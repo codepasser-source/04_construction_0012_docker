4e8f5bd622168eebb8426214282730866ad915b0
codepasser <codepasser@aliyun.com>
2023-06-28 14:58:26 +0800

    docker: consul single

--- source/docker/redis/single/docker-redis-cli.sh
+++ source/docker/redis/single/docker-redis-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-docker exec -it single.redis.codepasser.io redis-cli -p 6379
+docker exec -it single.redis.codepasser.io redis-cli -p 6379 -a cde3VFR$
