350a99719f773e65ad8b4d8acf75cdb77a60742c
codepasser <codepasser@aliyun.com>
2023-07-05 10:01:48 +0800

    docker: pulsar standalone

--- source/docker/pulsar/standalone/docker-pulsar-clean.sh
+++ source/docker/pulsar/standalone/docker-pulsar-clean.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # docker exec [OPTIONS] CONTAINER COMMAND [ARG...]
-docker volume rm standalone.pulsar.codepasser.io
+docker volume rm standalone.pulsar.codepasser.io.conf
+docker volume rm standalone.pulsar.codepasser.io.data
