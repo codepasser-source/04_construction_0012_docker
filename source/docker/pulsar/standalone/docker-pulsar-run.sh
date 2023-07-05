2a766b906787dcb46536197c401c475cdea1177f
codepasser <codepasser@aliyun.com>
2023-07-05 15:09:43 +0800

    docker: pulsar

--- source/docker/pulsar/standalone/docker-pulsar-run.sh
+++ source/docker/pulsar/standalone/docker-pulsar-run.sh
@@ -5,6 +5,7 @@ docker run \
   --restart=always \
   -p 6650:6650 \
   -p 8080:8080 \
+  -e PULSAR_PREFIX_advertisedAddress=standalone.pulsar.codepasser.io \
   -v standalone.pulsar.codepasser.io.conf:/pulsar/conf \
   -v standalone.pulsar.codepasser.io.data:/pulsar/data \
   -d codepasser/pulsar bin/pulsar standalone
