59af6bc869492d8beca3c42d04c60a342787cc45
codepasser <codepasser@aliyun.com>
2024-03-28 09:12:33 +0800

    docker: jenkins gogs es-head-node

---
+++ source/docker/nsenter/docker-nsenter-down.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# docker rmi [OPTIONS] IMAGE [IMAGE...]
+docker rmi -f codepasser/nsenter
---
+++ source/docker/nsenter/docker-nsenter-stop.sh
@@ -0,0 +1,4 @@
+# docker stop [OPTIONS] CONTAINER [CONTAINER...]
+docker stop nsenter.codepasser.io
+# docker rm [OPTIONS] CONTAINER [CONTAINER...]
+docker rm nsenter.codepasser.io
